78dfb8950646b5c6e63f6a6a61fb902da8b8c367
Dorival Rodrigues <dorivalcr@gmail.com>
2022-01-04 15:10:24 -0300

    Paginacao

---
+++ backend/src/main/java/com/projeto/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.projeto.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projeto.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
